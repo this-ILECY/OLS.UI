376f9db9e250fa57fe084054c79cfe47baa85a74
mardani.Amirhosein <amirhmch@hotmail.com>
2022-10-15 08:21:55 +0330

    OSL init

---
+++ src/app/mobile-bar/mobile-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-bar',
+  templateUrl: './mobile-bar.component.html',
+  styleUrls: ['./mobile-bar.component.scss']
+})
+export class MobileBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
